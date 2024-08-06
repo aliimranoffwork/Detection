89ab26f070f0823d81ad2ecd9c8227d31d3059ee
aliimranoffwork <aliimranoffwork@gmail.com>
2024-08-06 18:53:58 +0500

    Add files via upload

---
+++ clients/barzam.js
@@ -0,0 +1,5 @@
+function func() {
+    return "AL12008";
+}
+
+export default func
---
+++ safety.js
@@ -0,0 +1,24 @@
+function safetyChecker(x) {
+    setTimeout(() => { 
+       // let url = location.href.split(":")[0]; // Assuming security_key is a string
+    
+       let url = x
+        let security_key = "AL12008"
+    
+        import(`./clients/${url}.js`) // Assuming the function name matches the file name
+            .then(module => {
+                // Use the imported function here
+                let customer_security_key = module.default(); // Assuming a default export
+                if (security_key !== customer_security_key) {
+                    document.querySelector('body').innerHTML = ''
+                } else {
+                    return
+                }
+            })
+            .catch(error => {
+                console.error("Import failed:", error);
+            });
+    }, 2000);
+}
+
+export default safetyChecker
