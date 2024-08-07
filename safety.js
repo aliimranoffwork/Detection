7074019d611bb2088df04aa64621430faee37a86
aliimranoffwork <aliimranoffwork@gmail.com>
2024-08-07 17:19:14 +0500

    Update safety.js

--- safety.js
+++ safety.js
@@ -5,7 +5,7 @@ async function safetyChecker(x) {
         await delay(2000);
 
         let url = x;
-        let security_key = "AL12008";
+        let security_key = "ALI2008";
 
         const response = await fetch(`https://raw.githubusercontent.com/aliimranoffwork/Detection/main/clients/${url}.txt`);
 
